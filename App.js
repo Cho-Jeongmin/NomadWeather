019b165660f4de9136e255169192669ef12824da
Jeongmin Cho <jojm0829@gmail.com>
2023-02-25 01:27:49 +0900

    #2.4 Layout System

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN App!</Text>
-      <StatusBar style="light" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 5, backgroundColor: "orange" }}></View>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#234627",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 50,
-    color: "red",
-  },
-});
